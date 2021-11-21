ae99f1735f41ed08cccab3269690f700c3d925f5
Ishaan Takkar <63262408+takkar99@users.noreply.github.com>
2021-11-21 13:53:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
+echo "
+[general]n
+email = "ishaantakkar@gmail.com"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
 " > ~/.streamlit/config.toml
